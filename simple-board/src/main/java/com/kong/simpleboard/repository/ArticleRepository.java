48273e39f1bdb01e4292c046d391aa364f2c858c
Kong <rhdtn311@naver.com>
2022-08-12 17:19:43 +0900

    #1 - Querydsl 지원을 이용하여 api 검색 옵션 추가
    `QuerydslPredicateExecutor`, 
    `QuerydslBinderCustomizer`를 이용하여 간단하게 개발

--- simple-board/src/main/java/com/kong/simpleboard/repository/ArticleRepository.java
+++ simple-board/src/main/java/com/kong/simpleboard/repository/ArticleRepository.java
@@ -1,9 +1,29 @@
 package com.kong.simpleboard.repository;
 
 import com.kong.simpleboard.domain.Article;
+import com.kong.simpleboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>, // 기본적으로 Article 안에 있는 모든 필드에 대한 기본 검색 기능을 추가한다.
+        QuerydslBinderCustomizer<QArticle> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true);  // 선택적으로 검색하기 위해
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
